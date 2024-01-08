e70ad79d282531baae1a76e388b0a9053d310c02
TendaniSadiki <tendanisadiki16@gmail.com>
2024-01-08 19:23:52 +0200

    {forgot password] added

--- src/components/Constance/Constance.js
+++ src/components/Constance/Constance.js
@@ -1,47 +1,49 @@
-
+// Constance.js
 export const NumberInput = ({ label, value, onChange }) => (
     <div>
-        <label>{label}</label>
-        <input type="number" value={value} onChange={onChange} />
+      <label>{label}</label>
+      <input type="number" value={value} onChange={onChange} />
     </div>
-);
-
-export const TextArea = ({ label, value, onChange }) => (
+  );
+  
+  export const TextArea = ({ label, value, onChange }) => (
     <div>
-        <label>{label}</label>
-        <textarea value={value} onChange={onChange} />
+      <label>{label}</label>
+      <textarea value={value} onChange={onChange} />
     </div>
-);
-
-export const CheckboxInput = ({ label, checked, onChange }) => (
+  );
+  
+  export const CheckboxInput = ({ label, checked, onChange }) => (
     <label>
-        <input type="checkbox" checked={checked} onChange={onChange} />
-        {label}
+      <input type="checkbox" checked={checked} onChange={onChange} />
+      {label}
     </label>
-);
-
-export const FileInput = ({ label, accept, onChange }) => (
+  );
+  
+  export const FileInput = ({ label, accept, onChange }) => (
     <div>
-        <label>{label}</label>
-        <input type="file" accept={accept} onChange={onChange} />
+      <label>{label}</label>
+      <input type="file" accept={accept} onChange={onChange} />
     </div>
-);
-
-export const SelectInput = ({ label, value, options, onChange }) => (
+  );
+  
+  export const SelectInput = ({ label, value, options, onChange }) => (
     <div>
-        <label>{label}</label>
-        <select value={value} onChange={onChange}>
-            {options.map((option) => (
-                <option key={option.value} value={option.value}>
-                    {option.label}
-                </option>
-            ))}
-        </select>
+      <label>{label}</label>
+      <select value={value} onChange={onChange}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
     </div>
-);
-export const TextInput = ({ label, value, onChange }) => (
+  );
+  
+  export const TextInput = ({ label, value, onChange, placeholder }) => (
     <div>
-        <label>{label}</label>
-        <input type="text" value={value} onChange={onChange} />
+      <label>{label}</label>
+      <input type="text" value={value} onChange={onChange} placeholder={placeholder} />
     </div>
-);
+  );
+  
